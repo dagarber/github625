1778fa446e680534813c15bbae9c49d7f059bc51
dagarber <74267974+dagarber@users.noreply.github.com>
2021-11-06 23:08:19 -0400

    added MyShader and .cs script that adjusts object size and color to pass to shader

--- CgShader/Assets/Scripts/SpherePosition.cs
+++ CgShader/Assets/Scripts/SpherePosition.cs
@@ -7,11 +7,23 @@ public class SpherePosition : MonoBehaviour
 
     public GameObject sphere;
     private Renderer rend;
+    private float size = 1.0f;
+    private float sizeOrig;
+    private float factor = 0.5f;
+    private float factor2 = 0.0f;
+    private float r1 = 1.0f;
+    private float g1 = 0f;
+    private float b1 = 0f;
+    private float r2 = 0f;
+    private float g2 = 1.0f;
+    private float b2 = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
+        sizeOrig = size;
+        factor2 = 2.0f * factor;
 
         
     }
@@ -21,8 +33,29 @@ public class SpherePosition : MonoBehaviour
     {
         if(sphere != null)
         {
-            rend.sharedMaterial.SetVector(name: "_Point", sphere.transform.position);
+            Vector3 point = new Vector3(1.0f, size, 1.0f);
+            Vector3 color1 = new Vector3(r1, g1, b1);
+            Vector3 color2 = new Vector3(r2, g2, b2);
+            //rend.sharedMaterial.SetVector(name: "_Point", sphere.transform.position);
+            rend.sharedMaterial.SetVector(name: "_Point", point);
+            rend.sharedMaterial.SetVector(name: "_ColorNear", color1);
+            rend.sharedMaterial.SetVector(name: "_ColorFar", color2);
+
+            size = size - (size * factor * Time.deltaTime);
+            b1 = b1 + factor2 * Time.deltaTime;
+            r2 = r2 + factor2 * Time.deltaTime;
+
+            if (size < 0.5 * sizeOrig || size > sizeOrig)
+            {
+                factor = -1.0f * factor;
+                factor2 = -1.0f * factor2;
+                size = size - (size * factor * Time.deltaTime);
+                b1 = b1 + factor2 * Time.deltaTime;
+                r2 = r2 + factor2 * Time.deltaTime;
+            }
+
+
         }
-        
+
     }
 }
--- CgShader/Assets/Shaders/0-intro.shader
+++ CgShader/Assets/Shaders/0-intro.shader
@@ -1,4 +1,10 @@
 Shader "First Shader" {
+    Properties {
+        _Point ("point in world space", Vector) = (0,0,0,1)
+        _DistanceNear ("threshold distance", Float) = 3.0
+        _ColorNear ("color near", Color) = (1,0,0,1)
+        _ColorFar ("color far", Color) = (0,1,0,1)
+    }
     SubShader {
         Pass {
             CGPROGRAM
@@ -6,9 +12,12 @@ Shader "First Shader" {
             #pragma vertex vert
             #pragma fragment frag
 
+            uniform float4 _Point;
+
             float4 vert(float4 vertexPos: POSITION) : SV_POSITION {
                 // x, y, z plus w offset is the float4 input
-                return UnityObjectToClipPos(vertexPos *float4(1,0.5,1,1));
+                //return UnityObjectToClipPos(vertexPos *float4(1,0.5,1,1));
+                return UnityObjectToClipPos(vertexPos * _Point);
             }
 
             float4 frag(void) : COLOR {
