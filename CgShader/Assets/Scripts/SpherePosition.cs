270c832d5e2024fbed645ce0a11692d26d503b82
dagarber <74267974+dagarber@users.noreply.github.com>
2021-11-06 23:35:21 -0400

    updated shader colors

--- CgShader/Assets/Scripts/SpherePosition.cs
+++ CgShader/Assets/Scripts/SpherePosition.cs
@@ -17,11 +17,13 @@ public class SpherePosition : MonoBehaviour
     private float r2 = 0f;
     private float g2 = 1.0f;
     private float b2 = 0f;
+    private int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<Renderer>();
+        //rend = GetComponent<Renderer>();
+        rend = this.GetComponent<Renderer>();
         sizeOrig = size;
         factor2 = 2.0f * factor;
 
@@ -31,11 +33,31 @@ public class SpherePosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 point;
+        Vector3 color1;
+        Vector3 color2;
+
         if(sphere != null)
         {
-            Vector3 point = new Vector3(1.0f, size, 1.0f);
-            Vector3 color1 = new Vector3(r1, g1, b1);
-            Vector3 color2 = new Vector3(r2, g2, b2);
+            if(count < 2)
+            {
+                point = new Vector3(1.0f, size, 1.0f);
+                color1 = new Vector3(r1, g1, b1);
+                color2 = new Vector3(r2, g2, b2);
+            }
+            else if (count < 4)
+            {
+                point = new Vector3(size, 1.0f, 1.0f);
+                color1 = new Vector3(r1, b1, g1);
+                color2 = new Vector3(b2, g2, r2);
+            }
+            else
+            {
+                point = new Vector3(1.0f, 1.0f, size);
+                color1 = new Vector3(r1, g1, b1);
+                color2 = new Vector3(r2, g2, r2);
+            }
+
             //rend.sharedMaterial.SetVector(name: "_Point", sphere.transform.position);
             rend.sharedMaterial.SetVector(name: "_Point", point);
             rend.sharedMaterial.SetVector(name: "_ColorNear", color1);
@@ -45,6 +67,12 @@ public class SpherePosition : MonoBehaviour
             b1 = b1 + factor2 * Time.deltaTime;
             r2 = r2 + factor2 * Time.deltaTime;
 
+            if(count >= 4)
+            {
+                r1 = r1 - factor2 * Time.deltaTime;
+                g2 = g2 - factor2 * Time.deltaTime;
+            }
+
             if (size < 0.5 * sizeOrig || size > sizeOrig)
             {
                 factor = -1.0f * factor;
@@ -52,6 +80,18 @@ public class SpherePosition : MonoBehaviour
                 size = size - (size * factor * Time.deltaTime);
                 b1 = b1 + factor2 * Time.deltaTime;
                 r2 = r2 + factor2 * Time.deltaTime;
+                count++;
+
+                if (count >= 4)
+                {
+                    r1 = r1 - factor2 * Time.deltaTime;
+                    g2 = g2 - factor2 * Time.deltaTime;
+                }
+
+                if (count > 5)
+                {
+                    count = 0;
+                }
             }
 
 
