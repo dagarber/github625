a30f8ea4bd25eaa23f0948f9cf8158af968fb49d
garbeda1 <david.garber@jhuapl.edu>
2021-11-29 13:28:25 -0500

    added locomotion

---
+++ VRProject/Assets/Scripts/SphereReset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereReset : MonoBehaviour
+{
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > 2 || transform.position.z > 2)
+        {
+            transform.position = startPos;
+            GetComponent<Rigidbody>().velocity = new Vector3(0f,0f,0f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0f,0f,0f);
+		}
+        
+    }
+}
