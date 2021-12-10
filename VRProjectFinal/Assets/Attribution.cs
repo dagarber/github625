93a0ef09da559df6da2983dd859b4d89732bba08
Garber <david.garber@jhuapl.edu>
2021-12-10 18:33:20 -0500

    added additional attributions (sound, etc.)

--- VRProjectFinal/Assets/Attribution.cs
+++ VRProjectFinal/Assets/Attribution.cs
@@ -7,7 +7,14 @@ public class Attribution : MonoBehaviour
 
     //"Obi-Wan Kenobi Lightsaber" (https://skfb.ly/YTEK) by ivamargar is licensed under Creative Commons Attribution-NoDerivs (http://creativecommons.org/licenses/by-nd/4.0/).
     //"Flare Gun" (https://skfb.ly/6Anzu) by soidev is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-    
+    // Fire Sound from freeSound.org
+    // https://freesound.org/people/MATRIXXX_/sounds/415991/
+    // Launch Sound also from freeSound.org, no attribution required
+    // VR implementation based on YouTube tutorials by Valem
+    // 3D assets downloaded from sketchfab.com (links above)
+    // bird and piggy assets provided in class
+
+
     // Start is called before the first frame update
     void Start()
     {
